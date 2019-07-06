a2eb4be7995e430406245c5665df6d7965df0b2c
antoniogeroncio <antonio.santos.mcz@gmail.com>
2019-07-06 17:04:56 -0300

    Implementação das atividades 4 em andamento

--- Aula03-TestingTools copy/test/br/ufal/ic/testingtools/sort/SortTest.java
+++ Aula03-TestingTools copy/test/br/ufal/ic/testingtools/sort/SortTest.java
@@ -4,30 +4,47 @@ import static org.junit.Assert.*;
 
 import java.util.Random;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import br.ufal.ic.testingtools.sort.BubbleSort;
 import br.ufal.ic.testingtools.sort.Sort;
 
 public class SortTest {
+	
+	private static final int elementsSize = 1000000; 
 
-	@Test(timeout=1000)
-	public void testSort() {
-		final int elementsSize = 1000000; 
-		int[] elements = new int[elementsSize];
+	private int[] elements;
+	private Sort sortTest;
+
+	@Before
+	public void setUp() {
+		elements = new int[elementsSize];
 		Random generator = new Random();
-		Sort sortTest = new BubbleSort();
-		
 		for (int i = 0; i < elementsSize; ++i) {
 			elements[i] = generator.nextInt();
 		}
 		
-		sortTest.sort(elements);
-		
 		for (int i = 0; i < elements.length - 1; ++i) {
+			if(i==(elements.length -1)) {
+				throw Exception("Array ordened");
+			}
 			assertTrue("Element is not smaller", elements[i] <= elements[i + 1]);
 		}
-		
+	}
+
+	@Test(timeout=1000)
+	public void testSortPerformance() {
+		sortTest = new QuickSort();
+		sortTest.sort(elements);
+		assertEquals(elementsSize, elements.length);
+	}
+	
+	public void testSort() {
+		sortTest = new QuickSort();
+		sortTest.sort(elements);
+		assertEquals(elementsSize, elements.length);
 	}
 	
 }
